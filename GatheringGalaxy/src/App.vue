6e879c37f9f94adf57b470933f8292a2fcf0a8e7
lenartaron <allium.szinkron@gmail.com>
2024-11-12 19:12:55 +0100

    bejelentkezes

--- GatheringGalaxy/src/App.vue
+++ GatheringGalaxy/src/App.vue
@@ -1,9 +1,23 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <HelloWorld />
+  <header>
+    <img src="/src/assets/result.png" alt="GatheringGalaxy" title="Gathering Galaxy">
+    GatheringGalaxy
+  </header>
+  <main>
+    <form>
+      <input type="text" id="bejemail" placeholder="Email cím megadása">
+      <input type="password" id="bejpass" placeholder="Jelszó megadása">
+      <button id="bejgomb">Bejelentkezés</button>
+    </form>
+    <a href="">Van már fiokód</a>
+  </main>
+  <footer>
+    Copyright
+  </footer>
 </template>
 
 <style scoped>
